2571681f4ff869d7fc03a06601d96e2ce51f4345
yangd <13532023084@139.com>
2017-09-24 11:30:10 +0800

    添加iview-loader

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -13,21 +13,30 @@ module.exports = {
     module: {
         rules: [{
                 test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
+                use: [{
+                        loader: 'vue-loader',
+                        options: {
+                            loaders: {
 
-                        sass: ExtractTextPlugin.extract({
-                            use: ['css-loader?minimize', 'autoprefixer-loader', 'sass-loader'],
-                            fallback: 'vue-style-loader'
-                        }),
+                                sass: ExtractTextPlugin.extract({
+                                    use: ['css-loader?minimize', 'autoprefixer-loader', 'sass-loader'],
+                                    fallback: 'vue-style-loader'
+                                }),
 
-                        css: ExtractTextPlugin.extract({
-                            use: ['css-loader', 'autoprefixer-loader'],
-                            fallback: 'vue-style-loader'
-                        })
+                                css: ExtractTextPlugin.extract({
+                                    use: ['css-loader', 'autoprefixer-loader'],
+                                    fallback: 'vue-style-loader'
+                                })
+                            }
+                        }
+                    },
+                    {
+                        loader: 'iview-loader',
+                        options: {
+                            prefix: false
+                        }
                     }
-                }
+                ]
             },
             {
                 test: /iview\/.*?js$/,
