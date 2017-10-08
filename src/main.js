66002f9f2d32dbe922d417bc3dc74e1060582a9e
yangd <13532023084@139.com>
2017-10-09 00:00:59 +0800

    使用vue-cli重新初始化

--- src/main.js
+++ src/main.js
@@ -1,75 +1,15 @@
-import Vue from 'vue';
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
-import VueRouter from 'vue-router';
-import Routers from './router';
-import Vuex from 'vuex';
-import Util from './libs/util';
-import App from './app.vue';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
-import VueI18n from 'vue-i18n';
-import Locales from './locale';
-import zhLocale from 'iview/src/locale/lang/zh-CN';
-import enLocale from 'iview/src/locale/lang/en-US';
+Vue.config.productionTip = false
 
-Vue.use(VueRouter);
-Vue.use(Vuex);
-Vue.use(VueI18n);
-Vue.use(iView);
-
-// 自动设置语言
-const navLang = navigator.language;
-const localLang = (navLang === 'zh-CN' || navLang === 'en-US') ? navLang : false;
-const lang = window.localStorage.getItem('language') || localLang || 'zh-CN';
-
-Vue.config.lang = lang;
-
-// 多语言配置
-const locales = Locales;
-const mergeZH = Object.assign(zhLocale, locales['zh-CN']);
-const mergeEN = Object.assign(enLocale, locales['en-US']);
-Vue.locale('zh-CN', mergeZH);
-Vue.locale('en-US', mergeEN);
-
-
-// 路由配置
-const RouterConfig = {
-    mode: 'history',
-    routes: Routers
-};
-const router = new VueRouter(RouterConfig);
-
-router.beforeEach((to, from, next) => {
-    iView.LoadingBar.start();
-    Util.title(to.meta.title);
-    next();
-});
-
-router.afterEach(() => {
-    iView.LoadingBar.finish();
-    window.scrollTo(0, 0);
-});
-
-
-const store = new Vuex.Store({
-    state: {
-
-    },
-    getters: {
-
-    },
-    mutations: {
-
-    },
-    actions: {
-
-    }
-});
-
-
+/* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router: router,
-    store: store,
-    render: h => h(App)
-});
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
--- src/vendors.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
-import VueRouter from 'vue-router';
-import axios from 'axios';
-import Vuex from 'vuex';
-import echarts from 'echarts';
-
-import clipboard from 'clipboard';
